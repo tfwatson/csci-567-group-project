4f5520b2717cac1eecacc6b377c394705bc48c82
Yihan-zh <61449360+Yihan-zh@users.noreply.github.com>
2023-11-29 10:30:15 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -7,15 +7,9 @@ class Read_Csvfile:
         #filename should be the file location
         #the output will be array [ image name, tensor use as data, label]
 
-
         output = []
         with open(filename) as file_obj:
-            # Create reader object by passing the file
-            # object to reader method
             reader_obj = csv.reader(file_obj)
-
-            # Iterate over each row in the csv
-            # file using reader object
             for row in reader_obj:
                 print(row[1:-1])
                 tensor =  torch.Tensor(list(map(float, row[1:-1])))
